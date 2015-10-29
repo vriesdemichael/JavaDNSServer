469a69f7d692e6c47f04b69f1e8d83b22291593f
Stefan <stefan@stefantry>
2015-10-29 13:36:50 +0100

    Dont we need a something like a gitignore?

--- src/Gui.java
+++ src/Gui.java
@@ -1,6 +1,9 @@
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.util.Observable;
 import java.util.Observer;
 
@@ -29,6 +32,7 @@ public class Gui extends JFrame  {
 	JLabel amountReq;
 	Main m;
 	JList<String> list;
+	JButton addRecord;
 	
 	/**
 	 * Launch the application.
@@ -85,22 +89,24 @@ public class Gui extends JFrame  {
 		list.setBounds(10, 32, 358, 136);
 		contentPane.add(new JScrollPane(list));
 		
-		JButton deleteRecord = new JButton("Delete");
-		deleteRecord.setBounds(375, 51, 99, 23);
-		contentPane.add(deleteRecord);
-		
-		JButton saveButton = new JButton("Save Records");
-		saveButton.setBounds(375, 85, 99, 23);
-		contentPane.add(saveButton);
-		
 		textField = new JTextField();
 		textField.setBounds(10, 206, 358, 20);
 		contentPane.add(textField);
 		textField.setColumns(10);
 		
-		JButton btnNewButton = new JButton("Add Record");
-		btnNewButton.setBounds(375, 205, 89, 23);
-		contentPane.add(btnNewButton);
+		addRecord = new JButton("Add Record");
+		addRecord.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+				if(checkInput(textField.getText())) {
+					addLineToRecords(textField.getText());
+				} else {
+					System.out.println("I WANNA KNOW WHAT LOVE IS!!! I WANT YOU TO SHOOOOOOOOOOOW ME!");
+				}
+			}
+		});
+		addRecord.setBounds(375, 205, 89, 23);
+		contentPane.add(addRecord);
 		
 		JLabel lblAddRecordAs = new JLabel("Add record as: hostname ip. Eg: nu.nl 127 0 0 1");
 		lblAddRecordAs.setFont(new Font("Tahoma", Font.BOLD, 14));
@@ -147,5 +153,17 @@ public class Gui extends JFrame  {
 		contentPane.add(list);
 		
 		//list = new JList();
+	}
+	
+	private void addLineToRecords(String line) {
+		try {
+		    Files.write(Paths.get("records.txt"), line.getBytes(), StandardOpenOption.APPEND);
+		}catch (IOException e) {
+		    //exception handling left as an exercise for the reader
+		}
+	}
+	
+	private boolean checkInput(String s) {
+		return true;
 	}
 }
